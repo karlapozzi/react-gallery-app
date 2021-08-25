cb92cf4d37ce40d516a264f206c1f1f47aa2c3fa
karla <karla@getharvest.com>
2021-08-24 17:47:14 -0700

    Add Nav component

---
+++ src/Components/Nav.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Nav = props => (
+  <ul>
+    <li className="main-nav">
+      <a className="main-nav" href="">Cats</a>
+    </li>
+    <li className="main-nav">
+      <a className="main-nav" href="">Dogs</a>
+    </li>
+    <li className="main-nav">
+      <a className="main-nav" href="">Birds</a>
+    </li>
+  </ul>
+);
+
+export default Nav;
+
