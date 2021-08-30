fc8f1de00aea52f77553c0538ab0c1549f7612aa
karla <karla@getharvest.com>
2021-08-30 15:55:22 -0700

    Add a 404 page

---
+++ src/Components/NoPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const NoPage = props => (
+  <div>
+    <h3>Page Not Found</h3>
+    <p>Oops! The page you're looking for doesn't exist. Please double check the URL you entered.</p>
+  </div>
+);
+
+export default NoPage;
