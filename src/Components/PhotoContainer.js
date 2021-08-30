5a3ca0fb3963eb2f2556032eaf8247801bd386d9
karla <karla@getharvest.com>
2021-08-30 15:56:30 -0700

    Make PhotoContainer a class

--- src/Components/PhotoContainer.js
+++ src/Components/PhotoContainer.js
@@ -1,23 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Photo from './Photo';
 import NotFound from './NotFound';
 
-const PhotoContainer = props => {
-  
-  const results = props.data;
-  let images;
+export default class PhotoContainer extends Component {
 
-  if (results.length > 0) {
-  images = results.map(image => <Photo url={`https://live.staticflickr.com/${image.server}/${image.id}_${image.secret}_q.jpg`} key={image.id}/>)
-  } else {
-    images = <NotFound />
+  generatePhotos = (props) => {
+    const results = props.data;
+    let images;
+    if (results.length > 0) {
+      images = results.map(image => <Photo url={`https://live.staticflickr.com/${image.server}/${image.id}_${image.secret}_q.jpg`} key={image.id} description={image.title} />)
+      } else {
+        images = <NotFound />
+      }
+    return images;
   }
 
-  return (
-    <ul className="photo-container">
-      {images}
-    </ul>
-  );
-}
-
-export default PhotoContainer;
+  render () {
+    return (
+    <div className="photo-container">
+      <h2>{this.props.topic} images</h2>
+      <ul>
+      {this.generatePhotos(this.props)}
+      </ul>
+    </div>
+    );
+    }
+  }
