39f3e9114f43abab9aba605500783770b60f3519
karla <karla@getharvest.com>
2021-08-31 14:40:35 -0700

    Make PhotoContainer stateless

--- src/Components/PhotoContainer.js
+++ src/Components/PhotoContainer.js
@@ -1,28 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Photo from './Photo';
 import NotFound from './NotFound';
 
-export default class PhotoContainer extends Component {
+const PhotoContainer = props => {
 
-  generatePhotos = (props) => {
     const results = props.data;
     let images;
     if (results.length > 0) {
       images = results.map(image => <Photo url={`https://live.staticflickr.com/${image.server}/${image.id}_${image.secret}_q.jpg`} key={image.id} description={image.title} />)
-      } else {
-        images = <NotFound />
-      }
-    return images;
-  }
+    } else {
+      images = <NotFound />
+    }
 
-  render () {
     return (
     <div className="photo-container">
-      <h2>{this.props.topic} images</h2>
+      <h2>{props.topic} images</h2>
       <ul>
-      {this.generatePhotos(this.props)}
+      {images}
       </ul>
     </div>
     );
-    }
   }
+
+  export default PhotoContainer;
